d2d4a929961f1761f73b711945b245458767e010
rusok <rusok.software@gmail.com>
2026-04-08 19:19:13 -0400

    Primer commit

---
+++ src/main/java/com/pepe/ejemplokafka/EjemplokafkaApplication.java
@@ -0,0 +1,13 @@
+package com.pepe.ejemplokafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemplokafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemplokafkaApplication.class, args);
+	}
+
+}
